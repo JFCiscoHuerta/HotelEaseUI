1c507d042b2fd1db610586a809eca33610c93c3c
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-30 18:12:38 -0600

    chore: reeplace HttpCliento to HttpClientModule

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -6,7 +6,7 @@ import { RoomListComponent } from './components/room-list/room-list.component';
 import { RoomDetailsComponent } from './components/room-details/room-details.component';
 import { RoomFormComponent } from './components/room-form/room-form.component';
 import { NgxPaginationModule } from 'ngx-pagination';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
