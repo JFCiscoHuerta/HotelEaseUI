00fd1e4e90105bcd0d8fa5038c8ecba1ebeddbbb
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-30 18:20:45 -0600

    feat: add basic functionality to save a room

--- src/app/room/components/room-form/room-form.component.ts
+++ src/app/room/components/room-form/room-form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { RoomService } from '../../services/room.service';
+import { RoomRegisterDTO } from '../../models/room.model';
+import { RoomState } from '../../models/room-state.enum';
+import { RoomType } from '../../models/room-type.enum';
 
 @Component({
   selector: 'app-room-form',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class RoomFormComponent {
 
+  room: RoomRegisterDTO = {
+    roomNumber: 0,
+    priceByNight: 0,
+    roomState: "",
+    roomType: "",
+    roomImages: [],
+    roomFeatureIds: []
+  }
+
+  roomStates = Object.values(RoomState);
+  roomTypes = Object.values(RoomType);
+
+  constructor(private service: RoomService) {}
+
+  saveRoom(): void {
+    this.service.saveRoom(this.room).subscribe({
+      next: (savedRoom) => {
+        alert('Room saved successfully');
+      },
+      error: (error) => {
+        console.error(error)
+        alert('Failed to save the room. Please try again.')
+      }
+    })
+  }
 }
