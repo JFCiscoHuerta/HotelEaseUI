e223adc68779c4ac09fe280fe9cd8eee8f7bcd34
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-12-01 21:06:54 -0600

    feat: add room-feature paths

--- src/app/room/room-routing.module.ts
+++ src/app/room/room-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoomListComponent } from './components/room-list/room-list.component';
 import { RoomDetailsComponent } from './components/room-details/room-details.component';
 import { RoomFormComponent } from './components/room-form/room-form.component';
+import { RoomFeaturesListComponent } from './components/room-features-list/room-features-list.component';
+import { RoomFeaturesFormComponent } from './components/room-features-form/room-features-form.component';
 
 const routes: Routes = [
   {
@@ -12,6 +14,14 @@ const routes: Routes = [
   {
     path: 'update/:id',
     component: RoomFormComponent
+  },
+  {
+    path: 'features',
+    component: RoomFeaturesListComponent
+  },
+  {
+    path: 'features/create',
+    component: RoomFeaturesFormComponent
   },
   {
     path: ':id',
